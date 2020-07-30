9cc13ea6aa8578e96d98905e3adb02b743c88a77
mhyungdoo <mhyungdoo@gmail.com>
2020-07-30 12:22:52 +0900

    입문자를 위한 플러터(flutter) 튜토리얼

---
+++ android/app/src/main/kotlin/com/example/flutter_tutorial_20200730/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_tutorial_20200730
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: MyHomePage(title: 'Flutter Demo Home Page'));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0; // 언더바는 해당 클래스 내부에서만 사용하는 변수를 정의할 때 사용함.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("You have pushed the button how many times:"),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline3,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+}
